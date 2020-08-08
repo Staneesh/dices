580f8e9e91c33ddec3fe8229c6a58ce4e4974b7d
Michał Sala <0havaker@gmail.com>
2020-08-09 00:57:03 +0200

    Fix ports

--- game_server/src/main.rs
+++ game_server/src/main.rs
@@ -8,7 +8,7 @@ use std::env;
 async fn main() -> Result<(), Box<dyn Error>> {
     let addr = env::args()
         .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+        .unwrap_or_else(|| "0.0.0.0:2137".to_string());
 
     ws::listen(addr).await?;
 
