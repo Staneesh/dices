5c8e7b37414abac53dcd62464ec428be6950f672
Staneesh <blkreasons@outlook.com>
2020-08-08 23:48:02 +0200

    Added big CLI interface (hopefully works)

--- client_cli/src/main.rs
+++ client_cli/src/main.rs
@@ -1,11 +1,13 @@
-use client_lib::ServerConnector;
+use client_lib::game_manager::GameManager;
+use client_lib::server_connector::ServerConnector;
 use common::MessageFromServer;
-use std::net::{IpAddr, Ipv4Addr};
+use std::io;
 
 fn main() {
     println!("Welcome to DAJSES");
 
-    let mut server_connector = ServerConnector::new("127.0.0.1:3000").unwrap();
+    let mut server_connector = ServerConnector::new("127.0.0.1").unwrap();
+    let mut game_manager = GameManager::new("127.0.0.1").unwrap();
 
     loop {
         let wait_result = server_connector.wait_for_message();
@@ -17,19 +19,78 @@ fn main() {
                 round_number,
                 your_dices,
             } => {
-                println!("Init!");
+                println!("Welcome to round {:?}.", round_number);
+                println!("Players at the table: {:?}.", players);
+                println!("Your dices: {:?}", your_dices);
             }
 
             MessageFromServer::YourMove { username } => {
-                println!("YourMove!");
+                println!("Your move!");
+
+                let mut correct_move = 0;
+                let mut pick = String::new();
+                let mut picked_num = 0;
+
+                while correct_move == 0 {
+                    println!("Bet - 0 , Check - 1. What is Your move?");
+
+                    if let Ok(r1) = io::stdin().read_line(&mut pick) {
+                        if let Ok(number) = pick.parse::<i32>() {
+                            if number == 0 || number == 1 {
+                                correct_move = 1;
+                                picked_num = number;
+                                continue;
+                            }
+                        }
+                    }
+
+                    println!("Wrong input! Try again, please.");
+                }
+
+                assert!(correct_move == 1);
+
+                if picked_num == 0 {
+                    let mut correct_bet = 0;
+
+                    while correct_bet == 0 {
+                        println!("Place Your bet! (f.ex. 3x5)");
+                        let mut bet = String::new();
+                        if let Ok(r1) = io::stdin().read_line(&mut bet) {
+                            if bet.len() == 3 {
+                                let a_res = bet[0..1].parse::<u64>();
+                                let b = &bet[1..2];
+                                let c_res = bet[1..3].parse::<u64>();
+                                if a_res.is_ok() && a_res.is_ok() {
+                                    let a = a_res.unwrap();
+                                    let c = c_res.unwrap();
+
+                                    if a >= 0 && a <= 5 {
+                                        if b == "x" {
+                                            if c >= 0 && c <= 6 {
+                                                //correct!
+                                                correct_bet = 1;
+                                                game_manager.submit_bet(a, c);
+                                                continue;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        println!("Wrong input! Try again, please.");
+                    }
+                } else {
+                    println!("You checked!");
+                    game_manager.submit_check();
+                }
             }
 
             MessageFromServer::RoundEnd { loser } => {
-                println!("RoundEnd!");
+                println!("Round ended - {} lost!", loser);
             }
 
             MessageFromServer::GameEnd { winner } => {
-                println!("GameEnd!");
+                println!("Game ended - {} won!", winner);
             }
         }
     }
