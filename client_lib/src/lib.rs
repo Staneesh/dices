cd58b6e6d378f5492de8a2218648c6f4ce2d5fc9
cvybhu <>
2020-08-08 20:22:33 +0200

    Common messages and ServerConnector template

--- client_lib/src/lib.rs
+++ client_lib/src/lib.rs
@@ -1,3 +1,24 @@
+use common::MessageFromServer;
+
+struct ServerConnector
+{
+
+
+}
+
+impl ServerConnector
+{
+    fn new(server_address: std::net::IpAddr) -> Result<ServerConnector, String>
+    {
+        unimplemented!();
+    }
+
+    fn wait_for_message() -> Result<MessageFromServer, String>
+    {
+        unimplemented!();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ common/src/lib.rs
@@ -0,0 +1,39 @@
+
+pub enum MessageFromServer
+{
+    Init {
+        players: Vec<String>,
+        round_number: u64,
+        your_dices: Vec<u8>
+    },
+    YourMove {
+        username: String
+    },
+    RoundEnd {
+        loser: String
+    },
+    GameEnd {
+        winner: String
+    }
+}
+
+pub enum MessageFromClient
+{
+    IAm {
+        username: String,
+        token: String
+    },
+    Bet {
+        dices_count: u64,
+        number_on_dice: u64
+    },
+    Check
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
