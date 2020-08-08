0a1a248fa5ea7cdb0de32e46bcafaea505cd5efa
cvybhu <>
2020-08-08 23:43:16 +0200

    Start GameManager

--- client_lib/src/game_manager.rs
+++ client_lib/src/game_manager.rs
@@ -1,12 +1,26 @@
+use super::server_connector::ServerConnector;
+
 pub struct GameManager
 {
-
+    server_connector: ServerConnector
 }
 
 impl GameManager
 {
-    fn new() -> GameManager
+    fn new(server_address: &str) -> Result<GameManager, Box<dyn std::error::Error>>
     {
-        GameManager {}
+        Ok(GameManager {
+            server_connector: ServerConnector::new(server_address) ?
+        })
+    }
+
+    fn submit_bet(&mut self, dices_count: u64, number_on_dice: u64)
+    {
+
+    }
+
+    fn submit_check(&mut self)
+    {
+
     }
 }
